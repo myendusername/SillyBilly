a81bb3a79708417086ac48c6da1fd25316928e97
Frictic <bsovec60716@gmail.com>
2026-04-23 22:53:40 -0400

    Finished secondary ability for bobert
    Finished secondary ability for bobert, awesome!

--- Assets/Scripts/Ben/GasCloud.cs
+++ Assets/Scripts/Ben/GasCloud.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 public class GasCloud : MonoBehaviour
 {
     public GameObject explosionPrefab;
+    public GameObject flamePrefab;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Sparker"))
         {
             StartCoroutine(Explode());
+        }
+        else if (other.gameObject.CompareTag("Fire"))
+        {
+            StartCoroutine(Flame());
         }
     }
 
@@ -19,6 +24,15 @@ public class GasCloud : MonoBehaviour
         yield return wait;
 
         Instantiate(explosionPrefab, this.transform.position, this.transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator Flame()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+        yield return wait;
+
+        Instantiate(flamePrefab, this.transform.position, this.transform.rotation);
         Destroy(gameObject);
     }
 }
